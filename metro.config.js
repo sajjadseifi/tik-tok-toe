0704b92b11449f46e8ec472c925683b0be6413ec
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-08-15 18:51:56 +0430

    .

--- metro.config.js
+++ metro.config.js
@@ -2,20 +2,3 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 module.exports = getDefaultConfig(__dirname);
-
-// const { getDefaultConfig } = require("metro-config");
-
-// module.exports = (async () => {
-//   const {
-//     resolver: { sourceExts, assetExts }
-//   } = await getDefaultConfig();
-//   return {
-//     transformer: {
-//       babelTransformerPath: require.resolve("react-native-svg-transformer")
-//     },
-//     resolver: {
-//       assetExts: assetExts.filter(ext => ext !== "svg"),
-//       sourceExts: [...sourceExts, "svg"]
-//     }
-//   };
-// })();
